d0a46bd67a9372339518adcb8ff2650599652254
Aaron Luna <aaron.yamil@gmail.com>
2016-08-21 05:55:49 -0400

    push with correct user - project final ios module

---
+++ FindSmartPeople/personController.swift
@@ -0,0 +1,13 @@
+//
+//  personController.swift
+//  FindSmartPeople
+//
+//  Created by Diego Alejandro Orellana Lopez on 8/20/16.
+//  Copyright © 2016 Aaron Luna. All rights reserved.
+//
+
+import UIKit
+
+class PersonTableViewCell: UITableViewCell {
+    <#properties and methods#>
+}
